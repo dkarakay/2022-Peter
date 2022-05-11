800afcb6f3180f17173d56b64fc2c2aad0e65855
Deniz Karakay <deniz@karakay.me>
2022-05-11 18:28:06 +0300

    Peter project added

---
+++ mobile/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:peter/view/onboarding_page.dart';
+import 'firebase_options.dart';
+import 'helpers/constants.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(PeterApp());
+}
+
+class PeterApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peter',
+      theme: ThemeData(
+        primarySwatch: primaryTeal,
+        primaryColor: primaryTeal,
+        secondaryHeaderColor: Colors.orange,
+        hintColor: Colors.orange,
+      ),
+      home: OnboardingPage(),
+    );
+  }
+}
